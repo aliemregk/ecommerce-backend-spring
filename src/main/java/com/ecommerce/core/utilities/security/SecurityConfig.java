9d2158f254b02beb8532dc8d00680942e7eab3d8
aliemregk <aliemregk@outlook.com>
2022-12-06 17:35:36 +0300

    Add security configuration

---
+++ src/main/java/com/ecommerce/core/utilities/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.ecommerce.core.utilities.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final String[] PATTERNS = {
+            "/api/auth/register",
+            "/api/auth/login",
+            "/api/products/get*" };
+
+    // .antMatchers(HttpMethod.GET, "/api/author/**").permitAll()
+    // .antMatchers(HttpMethod.POST, "/api/author/search").permitAll()
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(PATTERNS)
+                .permitAll()
+                .anyRequest()
+                .authenticated().and()
+                .formLogin();
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+}
